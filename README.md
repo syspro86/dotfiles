6752df631f7e5d57ee8b3086388404f11d89afd7
syspro86 <syspro86@gmail.com>
2022-12-13 15:08:54 +0000

    git curl 설치 및 프로젝트 복제 추가

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # syspro86 dotfiles
 
+```
 bash <(curl -s https://raw.githubusercontent.com/syspro86/dotfiles/master/setup-github.sh)
+```
--- setup-github.sh
+++ setup-github.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -eu
 [ ! -e $HOME/.ssh/id_rsa.pub ] && ssh-keygen
-
 GITHUB_PUBKEY=`cat $HOME/.ssh/id_rsa.pub`
 
 read -p 'GitHub Personal Access Token: ' GITHUB_PAT
 echo
+
+# install curl git
+sudo apt install curl git
 
 curl -XPOST \
   -H "Content-Type: application/json" \
   -H "Authorization: token $GITHUB_PAT" \
   -d "{\"title\": \"$USER@$(hostname)\", \"key\": \"$GITHUB_PUBKEY\"}" \
   "https://api.github.com/user/keys"
+
+# clone dotfiles
+[ ! -d ~/dotfiles ] && git clone git@github.com:syspro86/dotfiles ~/dotfiles

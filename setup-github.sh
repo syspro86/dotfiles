3325bd397a231553e9ecfc70025af15440815334
syspro86 <syspro86@gmail.com>
2022-12-13 15:18:06 +0000

    github token 미입력시 등록 건너뜀

--- setup-github.sh
+++ setup-github.sh
@@ -3,17 +3,20 @@ set -eu
 [ ! -e $HOME/.ssh/id_rsa.pub ] && ssh-keygen
 GITHUB_PUBKEY=`cat $HOME/.ssh/id_rsa.pub`
 
-read -p 'GitHub Personal Access Token: ' GITHUB_PAT
-echo
-
 # install curl git
-sudo apt install curl git
+# sudo apt install -y curl git
 
+read -p 'GitHub Personal Access Token: ' GITHUB_PAT
+
+if [ "$GITHUB_PAT" == "" ]; then
+echo "skipping to add ssh key"
+else
 curl -XPOST \
   -H "Content-Type: application/json" \
   -H "Authorization: token $GITHUB_PAT" \
   -d "{\"title\": \"$USER@$(hostname)\", \"key\": \"$GITHUB_PUBKEY\"}" \
   "https://api.github.com/user/keys"
+fi
 
 # clone dotfiles
 [ ! -d ~/dotfiles ] && git clone git@github.com:syspro86/dotfiles ~/dotfiles

42521fd0f289f42e0e1a2c86a1ee9d047f1182e8
syspro86 <syspro86@gmail.com>
2022-12-20 20:56:32 +0900

    install d2coding font

--- install-zsh.sh
+++ install-zsh.sh
@@ -10,3 +10,12 @@ fi
 [ -f ~/.zshrc ] && rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 
+# install d2coding font
+font_wc=$(fc-list | grep 'D2Coding' | wc -l)
+if [ "$font_wc" = "0" ]; then
+    mkdir ~/.local/share/fonts
+    cd ~/.local/share/fonts
+    wget https://github.com/naver/d2codingfont/releases/download/VER1.3.2/D2Coding-Ver1.3.2-20180524.zip
+    unzip D2Coding-Ver1.3.2-20180524.zip
+    fc-cache -f -v
+fi

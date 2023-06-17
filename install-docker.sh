1f21f780db460ce2f51161f96668496b259d23d9
syspro86 <syspro86@gmail.com>
2023-06-17 11:33:24 +0900

    docker group 에 추가

--- install-docker.sh
+++ install-docker.sh
@@ -8,3 +8,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $(whoami)

bcc03424859199ad551c12d31d08c30164519408
Kiho Kim <syspro86@gmail.com>
2025-06-24 00:22:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@
 # install zsh vi mode
 [ "${ZSH_CUSTOM}" = "" ] && ZSH_CUSTOM=~/.oh-my-zsh/custom
 [ ! -d ${ZSH_CUSTOM}/plugins/zsh-vi-mode ] && git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM}/plugins/zsh-vi-mode
+
+# create symlinks
+[ -f ~/.zshrc ] && rm ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+[ ! -f ~/.gitconfig ] && ln -s ~/dotfiles/.gitconfig ~
